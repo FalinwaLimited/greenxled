14e2e240070c8f2e4939c1a87755ed8b5702e9a7
Randy Raharjo <randyraharjo93@gmail.com>
2016-12-13 10:04:52 +0700

    [BASE] COA CN, Payroll CN

---
+++ fal_l10n_cn_payroll/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "HRD-08_Payroll China Falinwa",
+    "version": "1.0",
+    'author': 'Falinwa Hans',
+    "description": """
+    Module to developed Odoo payroll based on Falinwa standard.
+    """,
+    "depends" : ['hr_payroll','account'],
+    'init_xml': [],
+    'data': [
+    ],
+    'update_xml': [
+        'views/hr_payroll_report_view.xml',
+        'data/hr_payslip_workflow.xml',
+        'data/base_data.xml',
+        'data/hr_payroll_report.xml',
+        'views/hr_payroll_view.xml',
+    ],
+    'css': [],
+    'js' : [
+    ],
+    'qweb': [],
+    'installable': True,
+    'active': False,
+    'application' : False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
